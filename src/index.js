010798211d24f98c69e09288c48e2d92bf1a2138
黒田直樹 <naoki.kuroda@garage-standard.jp>
2020-12-06 19:34:40 +0900

    Add withAuthenticator

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Amplify from 'aws-amplify';
+Amplify.configure({
+  Auth: {
+      identityPoolId: 'ap-northeast-1:7d48a52a-f59c-4218-8a54-4b3eb5d0aabe',
+      region: 'ap-northeast-1',
+      userPoolId: 'ap-northeast-1_m2jC1w0se',
+      userPoolWebClientId: '1t1rvpden51ggpj10mepbq0mgr',
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
